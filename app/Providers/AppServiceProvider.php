3966f2dac87bc00d3127c22e2d729051cc5a2a93
sazzad7299 <sazzadurrahman580@gmail.com>
2022-10-27 00:08:24 +0600

    Update Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
         $header_categories=Category::orderBy('id','desc')->get();
         view()->share('header_categories', $header_categories);
     }
